9fcac9a96da585e032de2ff848d394f9528e5abc
Roberto Núñez <robertonc1599@gmail.com>
2023-06-18 21:46:07 -0500

    Validaciones terminadas

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -12,7 +12,13 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    public function store(){
-        dd("Post...");
+    public function store(Request $request){
+        $this->validate($request,[
+            'name' => 'required|max:30',
+            'username' => 'required|unique:users|min:3|max:20',
+            'email' => 'required|unique:users|email|max:60',
+            'password' => 'required',
+            'password_confirmation' => 'required'
+        ]);
     }
 }
