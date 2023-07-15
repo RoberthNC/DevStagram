62a0f8e42457457d87c9d202f00e070d516ec7ec
Roberto Núñez <robertonc1599@gmail.com>
2023-07-14 20:03:00 -0500

    Sección de editar perfil finalizada

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,10 +9,22 @@
     <div class="flex justify-center">
         <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
             <div class="w-8/12 lg:w-6/12 px-5">
-                <img src="{{ asset('img/usuario.svg') }}" alt="Imagen usuario">
+                <img src="{{ $user->imagen ? asset('perfiles') . "/" . $user->imagen : asset('img/usuario.svg') }}" alt="Imagen usuario">
             </div>
             <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:items-start md:justify-center py-10">
-                <p class="text-gray-700 text-2xl">{{ $user->username }}</p>
+                <div class="flex items-center gap-2">
+                    <p class="text-gray-700 text-2xl">{{ $user->username }}</p>
+    
+                    @auth
+                        @if ($user->id === auth()->user()->id)
+                            <a href="{{ route('perfil.index') }}" class="text-gray-500 hover:text-gray-600 cursor-pointer">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                </svg>                              
+                            </a>
+                        @endif
+                    @endauth
+                </div>
 
                 <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
                     0
@@ -25,7 +37,7 @@
                 </p>
 
                 <p class="text-gray-800 text-sm mb-3 font-bold">
-                    0
+                    {{ $user->posts->count() }}
                     <span class="font-normal"> Posts</span>
                 </p>
             </div>
@@ -36,7 +48,7 @@
         <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
         
         @if ($posts->count()>0)
-            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 px-4">
                 @foreach ($posts as $post)
                     <div>
                         <a href="{{ route('posts.show',["post" => $post, "user" => $user]) }}">
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\PerfilController;
 use App\Http\Controllers\PostCotroller;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -31,7 +32,9 @@ Route::get("/login",[LoginController::class, 'index'])->name("login");
 Route::post("/login",[LoginController::class, 'store']);
 Route::post("/logout",[LogoutController::class, 'store'])->name("logout");
 
-Route::get("/{user:username}",[PostCotroller::class, 'index'])->name("posts.index");
+Route::get("/editar-perfil",[PerfilController::class,'index'])->name("perfil.index");
+Route::post("/editar-perfil",[PerfilController::class,'store'])->name("perfil.store");
+
 Route::get("/posts/create",[PostCotroller::class, 'create'])->name("posts.create");
 Route::post("/posts",[PostCotroller::class,'store'])->name("posts.store");
 Route::get("/{user:username}/posts/{post}",[PostCotroller::class,'show'])->name("posts.show");
@@ -43,3 +46,5 @@ Route::post("/imagenes",[ImagenController::class, "store"])->name("imagenes.stor
 
 Route::post("/posts/{post}/like",[LikeController::class,'store'])->name("posts.likes.store");
 Route::delete("/posts/{post}/like",[LikeController::class,'destroy'])->name("posts.likes.destroy");
+
+Route::get("/{user:username}",[PostCotroller::class, 'index'])->name("posts.index");
